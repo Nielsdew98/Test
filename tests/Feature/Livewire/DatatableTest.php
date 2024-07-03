85e923f1e272c35dc42954a337a1e4847629716f
Niels De Witte <dewitte.niels@outlook.com>
2024-07-03 11:59:04 +0200

    add livewire tests

--- tests/Feature/Livewire/DatatableTest.php
+++ tests/Feature/Livewire/DatatableTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Livewire;
 
 use App\Livewire\Datatable;
+use App\Models\Category;
+use App\Models\Recipe;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -10,11 +12,43 @@ use Tests\TestCase;
 
 class DatatableTest extends TestCase
 {
+    use refreshDatabase, WithFaker;
     /** @test */
-    public function renders_successfully()
+    public function display_all_recipes()
     {
 
+        Recipe::factory()->count(10)->create(['is_hidden' => false]);
+
         Livewire::test(Datatable::class)
-            ->assertStatus(200);
+            ->assertViewHas('recipes', function ($recipes) {
+                return count($recipes) == 10;
+            });
+    }
+    /** @test */
+    public function display_all_recipes_for_category()
+    {
+        $category = Category::factory()->count(2)->create();
+
+        Recipe::factory()->count(5)->recycle($category->first())->create(['is_hidden' => false]);
+        Recipe::factory()->count(5)->recycle($category->last())->create(['is_hidden' => false]);
+
+        Livewire::withQueryParams(['categories' => [$category->first()->id]])
+            ->test(Datatable::class)
+            ->assertViewHas('recipes', function ($recipes) {
+                return count($recipes) == 5;
+            });
+    }
+
+    /** @test */
+    public function display_all_recipes_for_search()
+    {
+       Recipe::factory()->create(['is_hidden' => false, 'name' => 'test']);
+       Recipe::factory()->create(['is_hidden' => false, 'name' => 'random']);
+
+        Livewire::withQueryParams(['search' => 'tes'])
+            ->test(Datatable::class)
+            ->assertViewHas('recipes', function ($recipes) {
+                return count($recipes) == 1;
+            });
     }
 }
--- tests/Feature/Livewire/IngredientsChangerTest.php
+++ tests/Feature/Livewire/IngredientsChangerTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Livewire;
 
 use App\Livewire\IngredientsChanger;
+use App\Models\Ingredient;
+use App\Models\Recipe;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -15,5 +17,34 @@ class IngredientsChangerTest extends TestCase
     {
         Livewire::test(IngredientsChanger::class)
             ->assertStatus(200);
+    }
+
+    /** @test */
+    public function increment_servings()
+    {
+       $recipe = Recipe::factory()->create();
+       $ingredient = Ingredient::factory()->recycle($recipe)->create();
+
+        Livewire::test(IngredientsChanger::class)
+            ->set('ingredients', [$ingredient])
+            ->set('servings', 2)
+            ->call('incrementServings')
+            ->assertSet('servings', 3);
+    }
+
+    /** @test */
+    public function decrement_servings()
+    {
+        $recipe = Recipe::factory()->create();
+        $ingredient = Ingredient::factory()->recycle($recipe)->create();
+        $updatedIngredient = $ingredient;
+        $updatedIngredient->amount = $ingredient->amount * (1/2);
+        $updatedIngredient->save();
+
+        Livewire::test(IngredientsChanger::class)
+            ->set('ingredients', [$ingredient])
+            ->set('servings', 2)
+            ->call('decrementServings')
+            ->assertSet('servings', 1);
     }
 }
